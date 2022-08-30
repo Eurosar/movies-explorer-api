52e9a26f3b14d0197594e023e7dbbb78e6c60eb1
Eurosar <euro.saratov@gmail.com>
2022-08-30 21:40:00 +0300

    Feat: Добавлен cors

--- app.js
+++ app.js
@@ -8,16 +8,18 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routes = require('./routes/index');
 const errorHandler = require('./middlewares/ErrorHandlingMiddleware');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const rateLimiter = require('./utils/rateLimiter');
+const corsOptions = require('./utils/cors');
 
 // Возьмем нужные данные из файла .env
 // Если файла .env не будет, то PORT будет равен 3000
 const { PORT = 3000, MONGO_DB } = process.env;
 
-// Подключим базу даных
+// Подключим базу данных
 mongoose.connect(MONGO_DB);
 
 // Запустим приложение
@@ -26,10 +28,13 @@ const app = express();
 // Валидация заголовков
 app.use(helmet());
 
+// Запускаем проверку CORS
+app.use(cors(corsOptions));
+
 // Ограничим число запросов с одного IP
 app.use(rateLimiter);
 
-// Запустим парсеры
+// Запустим parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -37,7 +42,7 @@ app.use(cookieParser());
 // Запишем логи запросов
 app.use(requestLogger);
 
-// Используем роуты
+// Используем routes
 app.use(routes);
 
 // Запишем логи ошибок
---
+++ utils/cors.js
@@ -0,0 +1,8 @@
+module.exports = {
+  origin: [
+    'https://eurosar2movies.nomoredomains.sbs',
+    'http://eurosar2movies.nomoredomains.sbs',
+    'http://localhost:3000',
+  ],
+  credentials: true,
+};
