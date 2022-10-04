0ee9e92639249010e921bfb689c0bcbb105739e2
Eurosar <euro.saratov@gmail.com>
2022-10-04 20:02:14 +0300

    Fix: Поправлен corsoptions

--- utils/cors.js
+++ utils/cors.js
@@ -1,7 +1,6 @@
 module.exports = {
   origin: [
     'https://eurosar2movies.nomoredomains.sbs',
-    'http://eurosar2movies.nomoredomains.sbs',
     'http://localhost:3000',
   ],
   credentials: true,
